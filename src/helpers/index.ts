c03ae69500c955b0734a0a4ce456771156af9057
Robert Westenberger <robert.westenberger@healthtrio.com>
2023-02-01 10:45:15 -0700

    Continue with intro

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export {createShader} from './createShader';
 export {createProgram} from './createProgram';
+export {resizeCanvasToDisplaySize} from './resizeCanvasToDisplaySize';
--- src/shaders/index.ts
+++ src/shaders/index.ts
@@ -2,17 +2,35 @@ export const vertexShaderSource = `#version 300 es
  
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
-in vec4 aPosition;
+// Our vertex shader's a_position attribute is a vec4, a
+// vector of 4 single-precision floating-point numbers.
+// In JS, it would be something like a_position = {x: 0, y: 0, z: 0, w: 0}
+in vec4 a_position;
  
 // all shaders have a main function
 void main() {
  
   // gl_Position is a special variable a vertex shader
   // is responsible for setting
-  gl_Position = aPosition;
+  gl_Position = a_position;
 }
 `;
 
+
+export const vertexShader2DSource = `#version 300 es
+attribute vec2 a_position;
+uniform vec2 u_resolution;
+ 
+void main() {
+  // convert the position from pixels to 0.0 to 1.0
+  vec2 zeroToOne = a_position / u_resolution;
+  // convert from 0->1 to 0->2
+  vec2 zeroToTwo = zeroToOne * 2.0;
+  // convert from 0->2 to -1->+1 (clip space)
+  vec2 clipSpace = zeroToTwo - 1.0;
+  gl_Position = vec4(clipSpace, 0, 1);
+}
+`;
 export const fragmentShaderSource = `#version 300 es
  
 // fragment shaders don't have a default precision so we need
