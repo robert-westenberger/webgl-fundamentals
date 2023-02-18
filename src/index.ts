a6099abb0213ddf8b0dc7cd89bcbd5b1d3ec2d5a
robert-westenberger <robert.michael.westenberger@gmail.com>
2023-02-18 17:34:13 -0500

    2d_translation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import {sec1} from './sec1';
 import {twoDSpace} from './2dspace';
-import {twoDSpaceTWGL} from "./2dspace_twgl";
+import {twoDTranslation} from "./2d_translation";
+// import {twoDSpaceTWGL} from "./2dspace_twgl";
 // import {sec1_2d} from './sec1_2d';
 // import {sec1_dynamicRectangles} from './sec1_dynamic_rectangles'
 // import {translate_rectangles} from "./translate_rectangles";
 // twoDSpace();
-twoDSpaceTWGL();
+twoDTranslation();
 // sec1();
 // sec1_2d();
 // translate_rectangles();
