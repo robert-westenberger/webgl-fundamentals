62a1bb2b952412f8a217af402dd07abf5b977a59
robert-westenberger <robert.michael.westenberger@gmail.com>
2023-02-20 18:00:49 -0500

    WIP 2d 3d

--- src/3d/orthographic-3d.ts
+++ src/3d/orthographic-3d.ts
@@ -7,38 +7,21 @@ import {
     drawBufferInfo,
     Arrays
 } from "twgl.js";
-import {mat3} from "gl-matrix";
+import {mat4} from "gl-matrix";
 import {create2DRotationInput} from "../helpers";
 
 
 
 const vertexShaderSource = `#version 300 es
-
-// an attribute is an input (in) to a vertex shader.
-// It will receive data from a buffer
-in vec2 a_position;
-
-// Used to pass in the resolution of the canvas
-uniform vec2 u_resolution;
-
+in vec4 a_position;
+ 
 // A matrix to transform the positions by
-uniform mat3 u_matrix;
-
+uniform mat4 u_matrix;
+ 
 // all shaders have a main function
 void main() {
   // Multiply the position by the matrix.
-  vec2 position = (u_matrix * vec3(a_position, 1)).xy;
-
-  // convert the position from pixels to 0.0 to 1.0
-  vec2 zeroToOne = position / u_resolution;
-
-  // convert from 0->1 to 0->2
-  vec2 zeroToTwo = zeroToOne * 2.0;
-
-  // convert from 0->2 to -1->+1 (clipspace)
-  vec2 clipSpace = zeroToTwo - 1.0;
-
-  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  gl_Position = u_matrix * a_position;
 }
 `;
 
@@ -55,7 +38,12 @@ void main() {
   outColor = u_color;
 }`;
 
-
+function degrees_to_radians(degrees: number)
+{
+    var pi = Math.PI;
+    return degrees * (Math.PI/180);
+}
+const degreesToRadians = (degrees:number) => degrees * (Math.PI/180);
 export function orthographic3D() {
     const canvas =<HTMLCanvasElement> document.getElementById("c");
 
@@ -70,65 +58,101 @@ export function orthographic3D() {
 
 
     const programInfo = createProgramInfo(gl, [vertexShaderSource, fragmentShaderSource]);
-    const translation = [50,50];
+    const translation = [250,200,50];
 
-    const angleInRadians = 0;
-    const c = Math.cos(angleInRadians);
-    const s = Math.sin(angleInRadians);
+    const angleX = degreesToRadians(40);
+    const angleY = degreesToRadians(25);
+    const angleZ = degreesToRadians(325);
+    const cX = Math.cos(angleX);
+    const sX = Math.sin(angleX);
+    const cY = Math.cos(angleY);
+    const sY = Math.sin(angleY);
+    const cZ = Math.cos(angleZ);
+    const sZ = Math.sin(angleZ);
     const sx = 1;
     const sy = 1;
+    const sz = 1;
+    const projectionWidth = (gl.canvas as HTMLCanvasElement).clientWidth;
+    const projectionHeight = (gl.canvas as HTMLCanvasElement).clientHeight;
+    const projectionDepth = 400;
+    const projectionMatrix = mat4.fromValues(
+        2 / projectionWidth, 0, 0, 0,
+        0, -2 / projectionHeight, 0, 0,
+        0, 0, 2 / projectionDepth, 0,
+        -1, 1, 0, 1
+    )
+    const translationMatrix = mat4.fromValues(
+        1,  0,  0,  0,
+        0,  1,  0,  0,
+        0,  0,  1,  0,
+        translation[0], translation[1], translation[2], 1,
+    );
+    const xRotationMatrix = mat4.fromValues(
+        1, 0, 0, 0,
+        0, cX, sX, 0,
+        0, -sX, cX, 0,
+        0, 0, 0, 1,
+    );
+    const yRotationMatrix = mat4.fromValues(
+        cY, 0, -sY, 0,
+        0, 1, 0, 0,
+        sY, 0, cY, 0,
+        0, 0, 0, 1,
+    );
+    const zRotationMatrix = mat4.fromValues(
+        cZ, sZ, 0, 0,
+        -sZ, cZ, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1,
+    );
+    const scalingMatrix = mat4.fromValues(
+        sx, 0,  0,  0,
+        0, sy,  0,  0,
+        0,  0, sz,  0,
+        0,  0,  0,  1,
+    );
+    let matrix = mat4.create();
 
-    const translationMatrix = mat3.fromValues(
-        1, 0, 0,
-        0, 1, 0,
-        translation[0], translation[1], 1,
-    );
-    const rotationMatrix = mat3.fromValues(
-        c,-s, 0,
-        s, c, 0,
-        0, 0, 1,
-    );
-    const scalingMatrix = mat3.fromValues(
-        sx, 0, 0,
-        0, sy, 0,
-        0, 0, 1,
-    );
-    let matrix = mat3.create();
-    matrix = mat3.multiply(matrix, translationMatrix, rotationMatrix);
-    matrix = mat3.multiply(matrix, matrix, scalingMatrix);
+    matrix = mat4.multiply(matrix, matrix, projectionMatrix);
+    matrix = mat4.multiply(matrix, matrix, translationMatrix);
+    matrix = mat4.multiply(matrix, matrix, xRotationMatrix);
+    matrix = mat4.multiply(matrix, matrix, yRotationMatrix);
+    matrix = mat4.multiply(matrix, matrix, zRotationMatrix);
+    matrix = mat4.multiply(matrix, matrix, scalingMatrix);
     const vertexAttributes: Arrays = {
         a_position: {
-            data: [// left column
-                0, 0,
-                30, 0,
-                0, 150,
-                0, 150,
-                30, 0,
-                30, 150,
+            data: [
+                // left column
+                0,   0,  0,
+                30,   0,  0,
+                0, 150,  0,
+                0, 150,  0,
+                30,   0,  0,
+                30, 150,  0,
 
                 // top rung
-                30, 0,
-                100, 0,
-                30, 30,
-                30, 30,
-                100, 0,
-                100, 30,
+                30,   0,  0,
+                100,   0,  0,
+                30,  30,  0,
+                30,  30,  0,
+                100,   0,  0,
+                100,  30,  0,
 
                 // middle rung
-                30, 60,
-                67, 60,
-                30, 90,
-                30, 90,
-                67, 60,
-                67, 90],
-            numComponents: 2
+                30,  60,  0,
+                67,  60,  0,
+                30,  90,  0,
+                30,  90,  0,
+                67,  60,  0,
+                67,  90,  0
+            ],
+            numComponents: 3
         } }
 
     const bufferInfoArray = createBufferInfoFromArrays(gl, vertexAttributes);
     resizeCanvasToDisplaySize(canvas);
     const uniforms = {
         u_matrix: matrix,
-        u_resolution: [gl.canvas.width, gl.canvas.height],
         u_color: [Math.random(), Math.random(), Math.random(), 1],
     };
 
