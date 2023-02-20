9de99888863293346b5068a54d559c39975a15d7
robert-westenberger <robert.michael.westenberger@gmail.com>
2023-02-19 22:07:37 -0500

    Ghetto 2d

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,14 @@ import {twoDSpace} from './2d/2dspace';
 // import {twoDTranslation} from "./2d/2d_translation";
 // import {twoDRotation} from "./2d/2d_rotation";
 // import {twoDRotationInput} from "./2d/2d_rotation_input";
-import {twoDScale} from "./2d/2d_scale";
+import {twoDMatrix} from "./2d/2d_matrix";
+// import {twoDScale} from "./2d/2d_scale";
 // import {twoDSpaceTWGL} from "./2dspace_twgl";
 // import {sec1_2d} from './sec1_2d';
 // import {sec1_dynamicRectangles} from './sec1_dynamic_rectangles'
 // import {translate_rectangles} from "./translate_rectangles";
 // twoDSpace();
-twoDScale();
+twoDMatrix();
 // sec1();
 // sec1_2d();
 // translate_rectangles();
